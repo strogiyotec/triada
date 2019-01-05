23302b1b41a7fd4db2f973cc71f5d96001b7a793
Lollipop <lollipop@devadmin.com>
2019-01-05 14:59:05 +0600

    init commit

---
+++ src/main/java/io/triada/triada/TriadaApplication.java
@@ -0,0 +1,14 @@
+package io.triada.triada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriadaApplication.class, args);
+	}
+
+}
+
