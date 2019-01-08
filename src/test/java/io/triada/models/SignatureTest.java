3a745bf71a50ea29b53b3c803c4dc1b391ce1798
Lollipop <lollipop@devadmin.com>
2019-01-08 16:23:20 +0600

    add test for txn and signature

---
+++ src/test/java/io/triada/models/SignatureTest.java
@@ -0,0 +1,48 @@
+package io.triada.models;
+
+import io.triada.models.amount.TxnAmount;
+import io.triada.models.id.WalletId;
+import io.triada.models.key.RsaKey;
+import io.triada.models.sign.TriadaSignature;
+import io.triada.models.transaction.SignedTriadaTxn;
+import io.triada.models.transaction.ValidatedTxn;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.util.ResourceUtils;
+
+import java.util.Date;
+
+import static org.hamcrest.CoreMatchers.is;
+
+public final class SignatureTest extends Assert {
+
+    @Test
+    public void testSignAndValdiate() throws Exception {
+        final TriadaSignature sign = new TriadaSignature();
+        final RsaKey priv = new RsaKey(
+                ResourceUtils.getFile(
+                        this.getClass().getResource("/keys/pkcs8")
+                )
+        );
+        final RsaKey pub = new RsaKey(
+                ResourceUtils.getFile(
+                        this.getClass().getResource("/keys/id_rsa.pub")
+                )
+        );
+        final WalletId walletId = new WalletId();
+        final SignedTriadaTxn signedTxn = new SignedTriadaTxn(
+                new ValidatedTxn(
+                        123,
+                        new Date(),
+                        new TxnAmount(1000L),
+                        "NOPREFIX",
+                        new WalletId(),
+                        "Hello . This is sign test"
+                )
+                , priv, walletId);
+        assertThat(signedTxn.signature().length(), is(684));
+        assertThat(sign.valid(pub, walletId, signedTxn), is(true));
+
+
+    }
+}
---
+++ src/test/java/io/triada/models/TxnTest.java
@@ -0,0 +1,58 @@
+package io.triada.models;
+
+import com.google.gson.JsonObject;
+import io.triada.models.amount.TxnAmount;
+import io.triada.models.id.WalletId;
+import io.triada.models.transaction.ValidatedTxn;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.hamcrest.CoreMatchers.is;
+
+public final class TxnTest extends Assert {
+
+    private final Date now = new Date();
+    private final int id = 123;
+    private final String prefix = "NOPREFIX";
+    private final WalletId walletId = new WalletId();
+    private final String desc = "Test to see json";
+    private final TxnAmount txnAmount = new TxnAmount(1000L);
+
+    @Test
+    public void testJson() {
+
+
+        final ValidatedTxn validatedTxn = new ValidatedTxn(
+                id,
+                now,
+                txnAmount,
+                prefix,
+                walletId,
+                desc
+        );
+        final JsonObject jsonObject = validatedTxn.asJson();
+        assertThat(
+                jsonObject.get("id").getAsInt(),
+                is(id)
+        );
+        assertThat(
+                jsonObject.get("date").getAsLong(),
+                is(now.getTime())
+        );
+        assertThat(
+                jsonObject.get("amount").getAsLong(),
+                is(txnAmount.value())
+        );
+        assertThat(
+                jsonObject.get("prefix").getAsString(),
+                is(prefix)
+        );
+        assertThat(
+                jsonObject.get("details").getAsString(),
+                is(desc)
+        );
+
+    }
+}
