87ae375ddcfdeb64a98be4a5ac042e2a6483e2eb
Lollipop <lollipop@devadmin.com>
2019-01-26 00:11:29 +0600

    update hexnumber

---
+++ src/main/java/io/triada/text/HexNumber.java
@@ -0,0 +1,25 @@
+package io.triada.text;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public final class HexNumber implements Text {
+
+    /**
+     * Length of hex number
+     */
+    private final int length;
+
+    /**
+     * Number to convert to hex
+     */
+    private final long number;
+
+    @Override
+    public String asText() {
+        return String.format(
+                "%0" + this.length + "x",
+                this.number
+        ).replaceAll("^\\.{2}", "ff");
+    }
+}
