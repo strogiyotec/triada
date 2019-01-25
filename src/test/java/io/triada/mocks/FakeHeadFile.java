c47da5da60de60a248643f7893c16d0baeac048d
Lollipop <lollipop@devadmin.com>
2019-01-26 00:11:08 +0600

    add moke fake

---
+++ src/test/java/io/triada/mocks/FakeHeadFile.java
@@ -0,0 +1,29 @@
+package io.triada.mocks;
+
+import io.triada.models.id.LongId;
+import io.triada.models.key.RsaKey;
+import io.triada.models.wallet.TriadaWallet;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileWriter;
+
+public final class FakeHeadFile {
+
+    public File fakeHome(final LongId id) throws Exception {
+        final File tempFile = File.createTempFile("/tmp/", TriadaWallet.EXT);
+        tempFile.deleteOnExit();
+        try (final FileWriter writer = new FileWriter(tempFile)) {
+            final String head = String.join(
+                    System.lineSeparator(),
+                    "test",
+                    "322",
+                    id.toString(),
+                    new RsaKey(ResourceUtils.getFile(this.getClass().getResource("/keys/id_rsa.pub"))).asPublic(),
+                    "\r"
+            );
+            writer.append(head);
+        }
+        return tempFile;
+    }
+}
--- src/test/java/io/triada/mocks/FakeHome.java
+++ src/test/java/io/triada/mocks/FakeHome.java
@@ -1,15 +1,9 @@
 package io.triada.mocks;
 
 import io.triada.models.id.LongId;
-import io.triada.models.key.RsaKey;
 import io.triada.models.wallet.TriadaWallet;
 import io.triada.models.wallet.Wallet;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.util.ResourceUtils;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 
 /**
  * Fake home directory
@@ -17,21 +11,9 @@ import java.io.IOException;
 @Slf4j
 public final class FakeHome {
 
-
-    public Wallet createWallet(final LongId id, final int txns) throws IOException {
-        final File tempFile = File.createTempFile("/tmp/", TriadaWallet.EXT);
-        tempFile.deleteOnExit();
-        try (final FileWriter writer = new FileWriter(tempFile)) {
-            writer.append(//head
-                    String.join(
-                            System.lineSeparator(),
-                            "test",
-                            "322",
-                            id.toString(),
-                            new RsaKey(ResourceUtils.getFile(this.getClass().getResource("/keys/id_rsa.pub"))).asPublic()
-                    )
-            );
-        }
-        return new TriadaWallet(tempFile);
+    public Wallet createWallet(final LongId id, final int txns) throws Exception {
+        return new TriadaWallet(
+                new FakeHeadFile().fakeHome(id)
+        );
     }
 }
