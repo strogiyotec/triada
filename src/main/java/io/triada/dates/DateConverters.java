6008df0a59dafab4bde25f176709336f996c6859
strogiyotec <almas337519@gmail.com>
2019-02-01 11:16:16 +0600

    add date minus hours method

--- src/main/java/io/triada/dates/DateConverters.java
+++ src/main/java/io/triada/dates/DateConverters.java
@@ -2,9 +2,12 @@ package io.triada.dates;
 
 import lombok.experimental.UtilityClass;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
 import java.util.Date;
 
 @UtilityClass
@@ -16,5 +19,17 @@ public final class DateConverters {
 
     public LocalDateTime toLocalDateTime(final Date date) {
         return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+    }
+
+    public Date nowMinusYears(final int years) {
+        final Calendar cal = Calendar.getInstance();
+        Date today = cal.getTime();
+        cal.add(Calendar.YEAR, -1 * years);
+        return cal.getTime();
+    }
+
+    public Date nowMinusHours(final int hours) {
+        final Instant ins = Instant.now().minus(hours, ChronoUnit.HOURS);
+        return Date.from(ins);
     }
 }
