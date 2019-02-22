7dd7cf0d9d2f4a9c899499e86d15484c09892222
strogiyotec <almas337519@gmail.com>
2019-02-22 10:57:15 +0600

    add amount test

--- src/test/java/io/triada/models/TxnAmountTest.java
+++ src/test/java/io/triada/models/TxnAmountTest.java
@@ -19,7 +19,7 @@ public final class TxnAmountTest extends Assert {
     }
 
     @Test
-    public void testPrintWithManyDigits(){
+    public void testPrintWithManyDigits() {
         final TxnAmount walletAmount = new TxnAmount(new BigDecimal("0.12345678"));
         assertEquals("0.123", walletAmount.asText(3));
         assertEquals("0.1234", walletAmount.asText(4));
@@ -28,7 +28,7 @@ public final class TxnAmountTest extends Assert {
     }
 
     @Test
-    public void testCompareWithZero(){
+    public void testCompareWithZero() {
         final TxnAmount walletAmount = new TxnAmount(new BigDecimal("0.000001"));
         assertTrue(!walletAmount.zero());
         assertTrue(walletAmount.positive());
@@ -36,9 +36,20 @@ public final class TxnAmountTest extends Assert {
     }
 
     @Test
-    public void testParsedTridz(){
+    public void testParsedTridz() {
         final TxnAmount walletAmount = new TxnAmount(900_000_000L);
-        assertThat(walletAmount.toString(),is("0.20TRIADA"));
+        assertThat(walletAmount.toString(), is("0.20TRIADA"));
+    }
+
+    @Test
+    public void testAdd() {
+        assertThat(new TxnAmount(new BigDecimal("39.99")).add(new TxnAmount(new BigDecimal("39.99")).value()).asText(2), is("79.98"));
+    }
+
+    // TODO: 2/22/19 result is not correct now it's 0.02 need to fix
+    @Test
+    public void testSub() {
+        assertThat(new TxnAmount(new BigDecimal("39.99")).substract(new TxnAmount(new BigDecimal("39.98")).value()).asText(2), is("0.01"));
     }
 
 }
