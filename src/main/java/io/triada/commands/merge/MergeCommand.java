25de7342768f6cfc3fc783aeeba8f383ffa8383f
strogiyotec <almas337519@gmail.com>
2019-03-08 19:57:57 +0600

    delete merge interface, use valuable command instead

--- src/main/java/io/triada/commands/merge/MergeCommand.java
+++ src/main/java/io/triada/commands/merge/MergeCommand.java
@@ -1,12 +1,15 @@
 package io.triada.commands.merge;
 
+import io.triada.commands.ValuableCommand;
 import io.triada.models.id.LongId;
 
 import java.util.List;
 
-public final class MergeCommand implements Merge{
+public final class MergeCommand implements ValuableCommand<List<LongId>> {
+
+
     @Override
-    public List<LongId> ids() {
+    public List<LongId> run(final String[] argc) throws Exception {
         return null;
     }
 }
