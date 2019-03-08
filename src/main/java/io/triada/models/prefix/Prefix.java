f477709c188a1ebc4577b96941d743828a81b0c0
strogiyotec <almas337519@gmail.com>
2019-03-08 18:20:04 +0600

    add non argc method to prefix

--- src/main/java/io/triada/models/prefix/Prefix.java
+++ src/main/java/io/triada/models/prefix/Prefix.java
@@ -1,6 +1,20 @@
 package io.triada.models.prefix;
 
+/**
+ * Create prefix with fixed length
+ */
 public interface Prefix {
 
+    /**
+     * @param length Prefix length
+     * @return prefix
+     */
     String create(int length);
+
+    /**
+     * @return Prefix length with default length (8)
+     */
+    default String create() {
+        return this.create(8);
+    }
 }
