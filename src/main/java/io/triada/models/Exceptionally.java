85f87069752734578016809a5470c74f9d580f1b
strogiyotec <almas337519@gmail.com>
2019-03-09 14:12:37 +0600

    add exceptionally class

---
+++ src/main/java/io/triada/models/Exceptionally.java
@@ -0,0 +1,39 @@
+package io.triada.models;
+
+import lombok.experimental.UtilityClass;
+import org.jooq.lambda.fi.util.function.CheckedConsumer;
+import org.jooq.lambda.fi.util.function.CheckedSupplier;
+
+/**
+ * Wrap exceptions to boolean representation
+ */
+@UtilityClass
+public final class Exceptionally {
+
+    /**
+     * @param consumer Consumer
+     * @param obj      Object to consume
+     * @return True if exception was thrown
+     */
+    public boolean hasException(final CheckedConsumer<Object> consumer, final Object obj) {
+        try {
+            consumer.accept(obj);
+            return false;
+        } catch (final Throwable exc) {
+            return true;
+        }
+    }
+
+    /**
+     * @param supplier Supplier
+     * @return True if exception was thrown
+     */
+    public boolean hasException(final CheckedSupplier<Object> supplier) {
+        try {
+            supplier.get();
+            return false;
+        } catch (final Throwable exc) {
+            return true;
+        }
+    }
+}
