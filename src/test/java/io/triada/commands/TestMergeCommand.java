cfd413945fb24421de045efcb441bb1dea0124f6
strogiyotec <almas337519@gmail.com>
2019-03-15 15:33:23 +0600

    add test merge on top test

--- src/test/java/io/triada/commands/TestMergeCommand.java
+++ src/test/java/io/triada/commands/TestMergeCommand.java
@@ -5,8 +5,10 @@ import io.triada.commands.merge.MergeCommand;
 import io.triada.commands.pay.PayCommand;
 import io.triada.commands.remote.RemoteNodes;
 import io.triada.mocks.FakeHome;
+import io.triada.mocks.FakeKeys;
 import io.triada.models.amount.TxnAmount;
 import io.triada.models.id.LongId;
+import io.triada.models.key.RsaKey;
 import io.triada.models.wallet.CopiesFromFile;
 import io.triada.models.wallet.Wallet;
 import io.triada.models.wallet.Wallets;
@@ -26,6 +28,8 @@ import java.util.List;
 public final class TestMergeCommand extends Assert {
 
     public final FakeHome fakeHome = new FakeHome();
+
+    private final FakeKeys fakeKeys = new FakeKeys();
 
     @Rule
     public final TemporaryFolder folder = new TemporaryFolder();
@@ -68,6 +72,31 @@ public final class TestMergeCommand extends Assert {
         );
         assertEquals(1, modified.size());
         assertEquals(wallet.head().id(), modified.get(0));
+    }
 
+    @Test
+    public void testMergeCopyOnTop() throws Exception {
+        final Wallet wallet = this.fakeHome.createEagerWallet(LongId.ROOT);
+        final Path copiesPath = this.folder.newFolder(wallet.head().id()).toPath();
+        final CopiesFromFile copies = new CopiesFromFile(copiesPath);
+        copies.add(FileUtils.readFileToString(wallet.file(), StandardCharsets.UTF_8), HostAndPort.fromParts("good-host", 80), 5);
+        final RsaKey key = fakeKeys.privateKey();
+        wallet.substract(
+                new TxnAmount(new BigDecimal("9.99")),
+                "NOPREFIX",
+                new LongId(),
+                key
+        );
+        new MergeCommand(
+                new Wallets(wallet.file().getParentFile()),
+                new RemoteNodes(this.folder.newFile()),
+                copiesPath.getParent()
+        ).run(
+                new String[]{
+                        "-merge",
+                        "ids=" + wallet.head().id()
+                }
+        );
+        assertTrue(wallet.balance().less(0L));
     }
 }
