e8f02c4ecc06da9d883eb85007bf34f30f8f897d
strogiyotec <almas337519@gmail.com>
2019-03-22 22:46:35 +0600

    add doc to cli

--- src/main/java/io/triada/models/cli/CommandLineInterface.java
+++ src/main/java/io/triada/models/cli/CommandLineInterface.java
@@ -9,6 +9,11 @@ import java.io.IOException;
  */
 public interface CommandLineInterface<T> {
 
+    /**
+     * @param command Command to execute
+     * @return Result of given command
+     * @throws IOException if failed
+     */
     T executeCommand(String command) throws IOException;
 
 }
