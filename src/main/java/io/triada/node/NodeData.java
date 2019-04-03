0d289a3ab75dbaefbbae471b3624d5a7c68b951e
strogiyotec <almas337519@gmail.com>
2019-04-03 22:50:36 +0600

    add doc for node

--- src/main/java/io/triada/node/NodeData.java
+++ src/main/java/io/triada/node/NodeData.java
@@ -3,16 +3,39 @@ package io.triada.node;
 import io.triada.text.Text;
 
 public interface NodeData extends Text {
+
+    /**
+     * @return Node host
+     */
     String host();
 
+    /**
+     * @return Node port
+     */
     int port();
 
+    /**
+     * @return Errors of given node
+     */
     int errors();
 
+    /**
+     * @return Node's score
+     */
     int score();
 
+    /**
+     * @return Node is master
+     */
     boolean master();
 
+    /**
+     * @param host   Host
+     * @param port   Port
+     * @param errors Errors
+     * @param score  Score
+     * @return Text representation
+     */
     String asText(String host, int port, int errors, int score);
 
 }
