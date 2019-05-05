27d5b686b0cacd50d9caf710c30ea7b70a6a0bed
strogiyotec <almas337519@gmail.com>
2019-05-05 21:34:47 +0600

    add taxes doc

--- src/main/java/io/triada/models/tax/Tax.java
+++ src/main/java/io/triada/models/tax/Tax.java
@@ -7,20 +7,50 @@ import io.triada.text.Text;
 
 import java.util.Optional;
 
+/**
+ * Taxes of transaction
+ */
 public interface Tax extends Text {
 
+    /**
+     * @param details Transaction details
+     * @return True if transaction tax exists with given details
+     */
     boolean exists(String details);
 
+    /**
+     * @param rsaKey RsaKey to sign transaction
+     * @param score  Score of node to pay
+     * @return new Tax instance
+     * @throws Exception if failed
+     *                   Pay taxes to node with given score
+     */
     Tax pay(RsaKey rsaKey, Score score) throws Exception;
 
+    /**
+     * @return Amount of taxes to be paid
+     */
     long paid();
 
+    /**
+     * @return Current  Taxes debt
+     */
     long debt();
 
+    /**
+     * @return Last Tax payment
+     */
     Optional<SignedTransaction> last();
 
+    /**
+     * @param score Score of remote node
+     * @return Details of transaction to given score
+     */
     String details(Score score);
 
+    /**
+     * @return True if wallet in dept
+     */
     boolean inDept();
 
 }
